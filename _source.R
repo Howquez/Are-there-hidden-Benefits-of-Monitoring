00266a27e43a20d6298b5e16a4d81e8436763bad
Hauke Roggenkamp <Hauke2Roggenkamp@gmail.com>
2018-11-10 11:57:25 +0000

    render radix, still directory problems

--- _source.R
+++ _source.R
@@ -1,6 +1,8 @@
 # Install Packages -----
 listOfPackages <- c("knitr",
+                    "rmarkdown",
                     "bookdown",
+                    "radix",
                     "magrittr",
                     "highcharter",
                     "plotly"
@@ -17,18 +19,20 @@ for(package in listOfPackages){
 }
 
 # Analysis -----
-
+# getwd()
+# list.files("_Analysis/01_Code")
 source("_Analysis/01_Code/01_ReadData.R⁩")
 source("_Analysis/01_Code/02_Analysis.R⁩")
 source("_Analysis/01_Code/03_Visualizations.R⁩")
-
 
 # Create docs folder and nojekyll needed to publish the html output -----
 dir.create("docs", showWarnings = FALSE)
 file.create("docs/.nojekyll")
 
-# Render Book -----
+# Render -----
+# Book
 render_book("index.Rmd", output_format = "gitbook",   output_dir = "docs")
 render_book("index.Rmd", output_format = "pdf_book",  output_dir = "docs")
 render_book("index.Rmd", output_format = "epub_book", output_dir = "docs")
-
+# Radix
+render("_radix/radix.Rmd", "all")
